48122cf0ece73f2a66058f88ca4aeb34c5b37798
Votable57 <colejjohn10@proton.me>
2025-06-08 22:18:41 -0700

    New Init

--- scripts/gameplay/characters/Player.cs
+++ scripts/gameplay/characters/Player.cs
@@ -8,8 +8,6 @@ public partial class Player : CharacterBody2D
 {
 	[Export] public StateMachine StateMachine;
 
-	[Export] public double MoveSpeed = 0;
-
     public override void _Ready()
 	{
 		StateMachine.ChangeState(StateMachine.GetNode<State>("FreeRoam"));
