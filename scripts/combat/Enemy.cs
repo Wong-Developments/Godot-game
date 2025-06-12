8ab7b846711f15bbefb6f36975efff3bdc0731d6
Nathan <nathan.hilde1@gmail.com>
2025-06-12 01:59:22 -0700

    refactor: move player and enemy logic into separate nodes with individual scripts
    - Created separate Player and Enemy Node2D scenes, each with its own script
    - Moved attack and damage logic out of CombatManager
    - CombatManager now delegates turn logic to Player and Enemy methods

---
+++ scripts/combat/Enemy.cs
@@ -0,0 +1,27 @@
+using Game.Core;
+using Godot;
+using System;
+
+public partial class Enemy : Node2D
+{
+    [Export] public int MaxHealth = 100;
+    public int Health { get; private set; }
+
+    public override void _Ready()
+    {
+        Health = MaxHealth;
+    }
+
+    public int Attack()
+    {
+        Logger.Debug("Enemy attacks!");
+        return 10;  // damage dealt
+    }
+
+    public void TakeDamage(int amount)
+    {
+        Health = Mathf.Max(Health - amount, 0);
+        Logger.Info($"Enemy takes {amount} damage, HP is now {Health}");
+    }
+}
+
---
+++ scripts/combat/Player.cs
@@ -0,0 +1,27 @@
+using Game.Core;
+using Godot;
+using System;
+
+public partial class Player : Node2D
+{
+    [Export] public int MaxHealth = 100;
+    public int Health { get; private set; }
+
+    public override void _Ready()
+    {
+        Health = MaxHealth;
+    }
+
+    public int Attack()
+    {
+        Logger.Debug("Player attacks!");
+        return 10;  // damage dealt
+    }
+
+    public void TakeDamage(int amount)
+    {
+        Health = Mathf.Max(Health - amount, 0);
+        Logger.Info($"Player takes {amount} damage, HP is now {Health}");
+    }
+}
+
