d5a612dca4c256835fd06b02393d4b20915df223
Votable57 <colejjohn10@gmail.com>
2025-06-17 20:56:59 -0700

    Fixed monster animations

--- scripts/overworld/enemies/Monster/MonsterAnimation.cs
+++ scripts/overworld/enemies/Monster/MonsterAnimation.cs
@@ -22,19 +22,57 @@ public partial class MonsterAnimation : AnimatedSprite2D
 	{
 		ECharacterAnimation previousAnimation = ECharacterAnimation;
 
+		float angle = state.direction.Normalized().Angle();
+
 		switch (animationType)
 		{
 			case "walk":
-				if (state.direction == Vector2.Up) ECharacterAnimation = ECharacterAnimation.walk_up;
-				else if (state.direction == Vector2.Down) ECharacterAnimation = ECharacterAnimation.walk_down;
-				else if (state.direction == Vector2.Left) ECharacterAnimation = ECharacterAnimation.walk_left;
-				else if (state.direction == Vector2.Right) ECharacterAnimation = ECharacterAnimation.walk_right;
+				if (Mathf.Abs(state.direction.X) > Mathf.Abs(state.direction.Y))
+				{
+					if (state.direction.X > 0)
+					{
+						ECharacterAnimation = ECharacterAnimation.walk_right;
+					}
+					else
+					{
+						ECharacterAnimation = ECharacterAnimation.walk_left;
+					}
+				}
+				else
+				{
+					if (state.direction.Y > 0)
+					{
+						ECharacterAnimation = ECharacterAnimation.walk_down;
+					}
+					else
+					{
+						ECharacterAnimation = ECharacterAnimation.walk_up;
+					}
+				}
 				break;
 			case "idle":
-				if (ECharacterAnimation == ECharacterAnimation.walk_up) ECharacterAnimation = ECharacterAnimation.idle_up;
-				else if (ECharacterAnimation == ECharacterAnimation.walk_down) ECharacterAnimation = ECharacterAnimation.idle_down;
-				else if (ECharacterAnimation == ECharacterAnimation.walk_left) ECharacterAnimation = ECharacterAnimation.idle_left;
-				else if (ECharacterAnimation == ECharacterAnimation.walk_right) ECharacterAnimation = ECharacterAnimation.idle_right;
+				if (Mathf.Abs(state.direction.X) > Mathf.Abs(state.direction.Y))
+				{
+					if (state.direction.X > 0)
+					{
+						ECharacterAnimation = ECharacterAnimation.idle_right;
+					}
+					else
+					{
+						ECharacterAnimation = ECharacterAnimation.idle_left;
+					}
+				}
+				else
+				{
+					if (state.direction.Y > 0)
+					{
+						ECharacterAnimation = ECharacterAnimation.idle_down;
+					}
+					else
+					{
+						ECharacterAnimation = ECharacterAnimation.idle_up;
+					}
+				}
 				break;
 		}
 
